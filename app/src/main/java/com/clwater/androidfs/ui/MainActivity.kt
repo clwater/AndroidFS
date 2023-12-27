0d428ea0c5cd663787c49d91afde158e2edee819
clwater <gzb@clwater.top>
2023-12-27 15:18:09 +0800

    wip: pager

--- app/src/main/java/com/clwater/androidfs/ui/MainActivity.kt
+++ app/src/main/java/com/clwater/androidfs/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -15,15 +16,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -80,7 +89,7 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun FS() {
+    fun FSMain(){
         Box(modifier = Modifier
             .fillMaxSize()
             .background(color = BackgroundColor)) {
@@ -164,6 +173,44 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    @OptIn(ExperimentalFoundationApi::class)
+    @Composable
+    fun FS() {
+        var currentIndex by remember{
+            mutableStateOf(0)
+        }
+        val pagerState = rememberPagerState(
+            pageCount = {3},
+            initialPage = 1,
+            )
+
+        LaunchedEffect(currentIndex){
+            pagerState.animateScrollToPage(currentIndex)
+        }
+
+        HorizontalPager(state = pagerState,
+            onFocusChanged {
+                Log
+            }
+            ) { page ->
+            when(page){
+                0 ->
+                    Button(onClick = {
+                        currentIndex = 1
+                    }) {
+                        Text(text = "cl")
+                    }
+                1 -> FSMain()
+                2 ->             Text(
+                    text = "Page: $page",
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+
+        }
+
 
     }
 }
